6c747fc3212ec7ad6dda862751360b0dda345551
Bukovetskiy-Stanislav <sstas347@gmail.com>
2021-09-17 14:07:49 +0300

    Init

--- myawesomeblog/myawesomeblog/settings.py
+++ myawesomeblog/myawesomeblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9kxx*u6@%v6qvqk$u&44wazinzs!ez^79o6*fo_$nd9$0+h^%('
+SECRET_KEY = 'django-insecure-6t+h42czv20xo#g9t6s9b&_^67!%ur56a(v@sfd$xa2=n@pq^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
